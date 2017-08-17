d1ed7e1cad1bdde19fbefef8a96f45531401610f
Curtis Baker <curtisbaker3@gmail.com>
2017-08-17 09:09:58 -0400

    test

--- main.js
+++ main.js
@@ -12,11 +12,20 @@ function onNewTurn() {
 
     for (var i = 0; i < planets.length; i++) {
         planets[i].income = planets[i].population * .1 - 10 * planets[i].randomIncomeModifier - planets[i].expenses;
-        planets[i].invasionRisk = planets[i].invasionRisk * 1.2 + 2
+        var safetyModification = (100 - planets[i].safetyLevel) * Math.random() * .4 + Math.random() * 4 //modfication set to 20% of the current difference + 2
+
+        if (planets[i].safetyLevel > 0) {
+            if (planets[i].safetyLevel - safetyModification > 0) {
+                planets[i].safetyLevel -= safetyModification;
+            } else {
+                planets[i].safetyLevel = 0
+            }
+        }
 
         if (planets[i].population < planets[i].maxpopulation) {
-            if (planets[i].population + planets[i].population * .05 < planets[i].maxpopulation) {
-                planets[i].population = planets[i].population * 1.05;
+            tempPopIncrease = planets[i].population * .05
+            if (planets[i].population + tempPopIncrease < planets[i].maxpopulation) {
+                planets[i].population = planets[i].population + tempPopIncrease;
             } else {
                 planets[i].population = planets[i].maxpopulation
             }
@@ -70,18 +79,7 @@ function formatMoney(number) {
     return number.toLocaleString('us-en', { style: 'currency', currency: 'USD' });
 }
 
-const renderPlanet = (planet, i) => {
-    const pr = nextPlanetRequirements[planet.level + 1];
-    return `
-        <div class="table-row">
-            <div class="table-text two">${ planet.name }</div>
-            <div class="table-text right">${ planet.population.toFixed(0) }</div>
-            <div class="table-text right">${ formatMoney(planet.income) }</div>
-            <div class="table-text right">${ planet.maxpopulation.toFixed(0) }</div>
-            <div class="table-text right" style="display:flex;"><button onclick="onPlanetDevelopment(${i})">${ pr.name } (${ formatMoney(pr.cost) })</button></div>
-        </div>
-    `
-};
+
 
 function drawIncome(income) {
     document.getElementById('income').innerText = formatMoney(income);
@@ -101,6 +99,21 @@ function drawPlanets() {
     planetList.innerHTML = planets.map((p, i) => renderPlanet(p, i)).join('');
 };
 
+const renderPlanet = (planet, i) => {
+    const pr = nextPlanetRequirements[planet.level + 1];
+    return `
+        <div class="table-row">
+            <div class="table-text two">${ planet.name }</div>
+            <div class="table-text right">${ formatMoney(planet.income) }</div>            
+            <div class="table-text right">${ planet.population.toFixed(0) }</div>
+            <div class="table-text right">${ planet.maxpopulation.toFixed(0) }</div>
+            <div class="table-text right" style="display:flex;"><button onclick="onPlanetDevelopment(${i})">${ pr.name } (${ formatMoney(pr.cost) })</button></div>            
+            <div class="table-text half right">${ planet.safetyLevel.toFixed(0) }</div>      
+
+        </div>
+    `
+};
+
 function onSubmitPlanet() {
     var nameInput = document.getElementById('stringinput');
     var populationInput = document.getElementById('input');
@@ -116,7 +129,7 @@ function onSubmitPlanet() {
         level: 0,
         nextPlanetRequirements: _.clone(nextPlanetRequirements),
         expenses: 0,
-        invasionRisk: 0,
+        safetyLevel: 100,
         income: Number(population * .1 - 10 * randomIncomeModifier),    
         maxpopulation: Number(50 + Math.random() * 50)
     });
@@ -154,8 +167,6 @@ function erase() {
 
 var planets = [];
 var money = 100;
-var steelfactorycost = 50;
-var steelfactoryquantity = 0;
 var housecost = 100;
 var housequantity = 0;
 var houseimprovementfx = 50;
@@ -181,28 +192,38 @@ var nextPlanetRequirements = [{
 }, {
     name: 'Farm',
     cost: 30,
-    incomeCost: 3,
+    incomeCost: 4,
 }, {
     name: 'Aqueduct',
     cost: 50,
-    incomeCost: 5,
+    incomeCost: 9,
 }, {
     name: 'Hospital',
     cost: 100,
-    incomeCost: 8,
+    incomeCost: 18,
 }, {
     name: 'Library',
     cost: 200,
-    incomeCost: 10,
+    incomeCost: 26,
 }, {
     name: 'Market',
     cost: 300,
-    incomeCost: 13,
+    incomeCost: 40,
 }, ..._.range(100).map((n) => ({
     name: 'Factory ' + (n + 1), 
     cost: 300 + (n * n * 30), 
-    incomeCost: 15 + (n * 5)
+    incomeCost: 50 + (n * n * 5)
 }))];
+
+var turrets = [{
+    name: 'Default',
+    cost: 0,
+    incomeCost: 0
+}, {
+    name: 'Turret',
+    cost: 15,
+    incomeCost: 1
+}]
 
 if (localStorage.getItem('game')) {
     load();
